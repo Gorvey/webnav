71c55cf2221c6436678120632578d18e13de098c
zengzhe <Zeng_Zhe@outlook.com>
2024-06-16 17:36:36 +0800

    feat: update

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@
  * @Author: zengzhe
  * @Date: 2024-06-05 17:31:57
  * @LastEditors: zengzhe
- * @LastEditTime: 2024-06-05 18:10:03
+ * @LastEditTime: 2024-06-16 17:25:04
  * @Description:
  */
-import { getNavDataBase } from "@/lib/notion"
+import { getGroupData } from "@/lib/notion"
 import { LinkContent } from "@/components/link-content"
 import { Sidebar } from "@/components/sidebar"
 import { SiteFooter } from "@/components/site-footer"
@@ -16,25 +16,27 @@ import { SiteHeader } from "@/components/site-header"
 export const revalidate = 86400
 
 export default async function IndexPage() {
-  const groupData = await getNavDataBase()
+  const groupData = await getGroupData()
   // const navResources = await getNavLinks()
-  return (
-    <div className="container relative mx-auto min-h-screen w-full px-0">
-      {/* <div className="fixed z-20 hidden min-h-screen w-[16rem] transition-all duration-300 ease-in-out sm:block ">
-        </div> */}
-      <div className="flex-1 ">
-        <SiteHeader navItems={groupData} />
-        <div className="flex">
-          <div className="hidden w-[18rem] shrink-0  sm:block ">
-            <div className="fixed  min-h-screen  transition-all duration-300 ease-in-out">
-              <Sidebar navItems={groupData} />
-            </div>
-          </div>
-          <LinkContent navResources={groupData} />
-        </div>
+  console.log(groupData)
+  return <>123</>
+  // return (
+  //   <div className="container relative mx-auto min-h-screen w-full px-0">
+  //     {/* <div className="fixed z-20 hidden min-h-screen w-[16rem] transition-all duration-300 ease-in-out sm:block ">
+  //       </div> */}
+  //     <div className="flex-1 ">
+  //       <SiteHeader navItems={groupData} />
+  //       <div className="flex">
+  //         <div className="hidden w-[18rem] shrink-0  sm:block ">
+  //           <div className="fixed  min-h-screen  transition-all duration-300 ease-in-out">
+  //             <Sidebar navItems={groupData} />
+  //           </div>
+  //         </div>
+  //         <LinkContent navResources={groupData} />
+  //       </div>
 
-        <SiteFooter />
-      </div>
-    </div>
-  )
+  //       <SiteFooter />
+  //     </div>
+  //   </div>
+  // )
 }
--- lib/notion.ts
+++ lib/notion.ts
@@ -1,35 +1,66 @@
+/*
+ * @Author: zengzhe
+ * @Date: 2024-06-03 16:48:13
+ * @LastEditors: zengzhe
+ * @LastEditTime: 2024-06-16 17:27:09
+ * @Description:
+ */
 import { Client } from '@notionhq/client'
 import { toGroupData } from './notion-help'
-import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints"
+import type { PageObjectResponse, GetDatabaseResponse } from "@notionhq/client/build/src/api-endpoints"
 
 const notion = new Client({
   auth: process.env.NOTIONAUTH,
 })
-const databaseID = 'ce5480c97868423aacca062b0e7bde04'
-export const getNavDataBase = () => {
-  return notion.databases.query({
-    database_id: process.env.NOTIONDATABASEID || '',
-    // filter: {
-    //   property: 'group',
-    //   select: {
-    //     equals: '资料\\博客',
-    //   },
-    // },
-  }).then(({ results }) => {
-    return toGroupData(results as PageObjectResponse[])
+const databaseID = '031599ced6c84dc7b88a0da1af64c53d'
+export const queryDataBaseAllList = async () => {
+  let allData = [] as PageObjectResponse[];
+  let hasMore = true;
+  let startCursor = undefined;
 
+  while (hasMore) {
+    const response = await queryDataBase({ start_cursor: startCursor });
+    allData = [...allData, ...response.results as PageObjectResponse[]];
+    hasMore = response.has_more;
+    startCursor = response.next_cursor || undefined;
+  }
+
+  return allData;
+};
+export const queryDataBase = async ({
+  start_cursor
+}: { start_cursor: string | undefined }) => {
+  return notion.databases.query({
+    database_id: databaseID,
+    start_cursor,
+    page_size: 100,
+    filter: {
+      property: 'is-hidden',
+      checkbox: {
+        "equals": false
+      }
+    },
+    sorts: [{
+      property: 'group',
+      direction: 'ascending'
+    }, {
+      property: 'order',
+      direction: 'ascending'
+    }]
   })
+
 }
-export const retrieveNavDataBase = () => {
-  return notion.databases.retrieve({
+export const retrieveDataBase = async () => {
+  let response = await notion.databases.retrieve({
     database_id: databaseID,
   })
-  //   .then(({properties})=>{
-  //     let group= Object.values(properties ).find(property=>{
-  //       return property.name === 'group'
-  //       })
-  //       if (group && group.type === 'select') {
-  //           return group.select;
-  //         }
-  //     })
+
+  return response.properties.group
+
+}
+
+export const getGroupData = async () => {
+  let groups = await retrieveDataBase()
+  let results = await queryDataBaseAllList()
+
 }
